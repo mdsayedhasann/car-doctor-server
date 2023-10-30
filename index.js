c6a56fd4a1d1886145d5371c398277ad69371761
sayed_hossain <sayed.hasan0011@gmail.com>
2023-10-30 17:20:49 +0600

    Service Booking POST and GET

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const app = express()
 const port = process.env.PORT || 5000
 
 // Mongo DB Importer
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config()
 // Mongo DB Importer
 
@@ -34,17 +34,83 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+
+    // Service Database 
+    const serviveCollection = client.db('carDoctor').collection('services')
+
+    // Booking Database
+    const bookingCollection = client.db('carDoctor').collection('booking')
+
+
+    // Service Start  
+    app.get('/services', async(req,res) => {
+        const cursor = serviveCollection.find()
+        const result = await cursor.toArray()
+        res.send(result)
+    })
+    // Service End  
+
+
+
+    // Booking Start  {{{{{{  POST  }}}}}}
+    app.post('/booking', async (req, res) => {
+        const booking = req.body
+        console.log(booking);
+
+        const result = await bookingCollection.insertOne(booking)
+        res.send(result)
+    })
+    // Booking End   {{{{{{  POST  }}}}}}
+
+
+    // Booking Data Get Start {{{{{{{   Get   }}}}}}}
+
+    app.get('/booking', async(req, res) => {
+        const cursor = bookingCollection.find()
+        const result = await cursor.toArray()
+        res.send(result)
+    })
+
+    // Booking Data Get End {{{{{{{   Get   }}}}}}}
+
+
+
+
+    // Load Service Data Start  {{{{{{{     FIND ONE      }}}}}}}
+    app.get('/services/:id', async(req, res) => {
+        const id = req.params.id; 
+        const query = {_id: new ObjectId(id)}
+        // Options Start
+
+        const options = {
+            // Include only the `title` and `imdb` fields in the returned document
+            projection: { title: 1, service_id: 1, img: 1, price: 1 },
+          };
+
+        // Options End
+        const result = await serviveCollection.findOne(query, options)
+        res.send(result)
+    })
+    // Load Service Data End  {{{{{{{     FIND ONE      }}}}}}}
+    
+
+
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
-    await client.close();
+    // await client.close();
   }
 }
 run().catch(console.dir);
 
 // MongoDB End
+
+
+
+
 
 app.get('/', (req, res) => {
     res.send('Doctor is running')
