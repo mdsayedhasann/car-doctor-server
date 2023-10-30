4631516067be2b94d9f5a948baea1d8e218709db
sayed_hossain <sayed.hasan0011@gmail.com>
2023-10-30 23:47:39 +0600

    Add services data read only this email info

--- index.js
+++ index.js
@@ -65,9 +65,20 @@ async function run() {
 
     // Booking Data Get Start {{{{{{{   Get   }}}}}}}
 
-    app.get('/booking', async(req, res) => {
-        const cursor = bookingCollection.find()
-        const result = await cursor.toArray()
+    // app.get('/booking', async(req, res) => {
+    //     const cursor = bookingCollection.find()
+    //     const result = await cursor.toArray()
+    //     res.send(result)
+    // })
+
+    // New Style 
+    app.get('/booking', async (req, res) => {
+        console.log(req.query.email);
+        let query = {}
+        if(req.query?.email){
+            query = {email: req.query.email}
+        }
+        const result = await bookingCollection.find().toArray()
         res.send(result)
     })
 
